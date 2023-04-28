7190a2b7d62f151f4f0038eefcbccf083c282ea5
Propietari Local <abbo004@vidalibarraquer.net>
2023-04-28 13:22:09 +0200

    c

---
+++ src/main/java/com/mycompany/testunitari/Calculadora.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.testunitari;
+
+/**
+ *
+ * @author admin
+ */
+public class Calculadora {
+    int num1;
+    int num2;
+    
+    public Calculadora(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    
+}
